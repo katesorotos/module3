ac96841c15e6b778f709b82be0680022713742ee
Kate Sorotos <kate_sorotos@live.co.uk>
2019-01-24 10:46:05 +0000

    updated password check with validation

--- ch02_validation/SimpleBundlePurchase_validation.py
+++ ch02_validation/SimpleBundlePurchase_validation.py
@@ -7,20 +7,27 @@ Created on Tue Dec 11 13:52:48 2018
 
 """ch02_Data Bundle Purchase_validation"""
 
+
 import time 
 
 def passwordCheck(truePasscode, balance):
     count = 3
-    while count > 0:
-        password_attempt = input('Please enter your password: ')
-        if password_attempt == truePasscode:
-            print('Password OK')
-            return DataBundlePurchase(truePasscode, balance)
-        else:
-            count = count -1
-            print(str('Password incorrect - You have ' + str(count) + ' attempts left'))
+    while True:
+        try:
+            while count > 0:
+                password_attempt = int(input('Please enter your password: '))
+                if password_attempt != truePasscode:
+                    count = count -1
+                    print(str('Password incorrect - You have ' + str(count) + ' attempts left'))
+                elif password_attempt == truePasscode:
+                    print('Password OK')
+                    DataBundlePurchase(truePasscode, balance)
+            break
+        except ValueError:
+            print('Error: please make your choice by entering a number')
     print('Transaction cancelled - You have used up all of your attempts')
     
+
 
 def DataBundlePurchase(truePasscode, balance):
         time.sleep(1)
@@ -44,18 +51,19 @@ def DataBundlePurchase(truePasscode, balance):
         
         if transactionType == 1:
             print('Your balance is: £{}'.format(balance))
+            
             print('Would you like another service? ')
             print('Type "y" for yes or "n" for no ')
             
-            restart = input('Please enter your choice: ')
+            restart = ""
             while True:
                 try:
-                    while restart == 'y' or restart == 'n':
+                    while restart != 'y' or restart != 'n':
+                        restart = input('Please enter your choice: ')
                         if restart == 'y':
                             DataBundlePurchase(truePasscode, balance)
                         elif restart == 'n':
                             print('Thanks, have a nice day!')
-                            return 'exit_transaction'
                     break
                 except ValueError:
                     print('Error: please make your choice by entering "y" or "n"')           
@@ -64,8 +72,6 @@ def DataBundlePurchase(truePasscode, balance):
             checkNumber(truePasscode, balance)
         elif transactionType == 3:
             topUp(truePasscode, balance)
-        else: 
-            print('this is a test')
         
 
 def retryPassword(truePasscode, balance):
@@ -75,9 +81,7 @@ def retryPassword(truePasscode, balance):
         DataBundlePurchase(truePasscode, balance)
     else:
         print('Thanks, have a nice day!')
-        return 'Retry password exit'
     
-   
     
 def checkBalance(balance):
     if balance > 0:
@@ -155,7 +159,7 @@ def topUp(truePasscode, balance):
         print('Thanks, have a nice day!')
         return 'Exit'
     
-passwordCheck('1234', 34.55)
+passwordCheck(1234, 34.55)
 
 
     
